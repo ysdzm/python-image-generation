d32257fe6d77e3cec05eb3f3af6d686c130b94cf
dokudami <ysdzm75@gmail.com>
2025-06-25 22:11:57 +0000

    fix: ユーザーが異なりファイル保存ができないため、vscodeユーザーでコンテナを利用するように修正

--- .devcontainer/post-create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt update
-apt install -y python3 python3-pip
-pip install --no-cache-dir ipykernel nbstripout
-python3 -m ipykernel install --user --name=myenv --display-name 'python-gpu-dev-container'
